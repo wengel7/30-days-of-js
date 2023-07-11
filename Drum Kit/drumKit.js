32fae14ace8343d829b5e06a0ca61534ffef114c
wengel7 <tesemawengel3@gmail.com>
2023-07-11 15:41:30 +0300

    first commit

---
+++ Drum Kit/drumKit.js
@@ -0,0 +1,47 @@
+// window.addEventListener('type:DOMString', callback: EventListener, capture?:boolean)
+let h = 0;
+window.addEventListener('keydown', function(e) {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+console.log(e.keyCode)
+    if(!audio) return; //no need to play any sound for this key press
+    else{
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add("playing");
+
+        if(h != e.keyCode){
+            audio = document.querySelector(`audio[data-key="${h}"]`);
+            key = document.querySelector(`div[data-key="${h}"]`);
+            h = e.keyCode;
+            if(!key)return;
+            else{
+                // audio.stop();
+                key.classList.remove("playing");
+            }
+        }
+    }
+});
+
+// window.addEventListener('click', ()=>{
+// console.log(" ,kljk");
+
+    // let audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+    // if(!audio) return; //no need to play any sound for this key press
+    // else{
+    //     audio.currentTime = 0;
+    //     audio.play();
+    //     key.classList.add("playing");
+
+    //     if(h != key.dataset.key){
+    //         audio = document.querySelector(`audio[data-key="${h}"]`);
+    //         key = document.querySelector(`div[data-key="${h}"]`);
+    //         h = key.dataset.key;
+    //         if(!key)return;
+    //         else{
+    //             // audio.stop();
+    //             key.classList.remove("playing");
+    //         }
+    //     }
+    // }
+// });
