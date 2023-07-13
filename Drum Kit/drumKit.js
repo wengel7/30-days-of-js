1b4b9cd52627eec1005fac135a4f0ad4e70a8349
wengel7 <tesemawengel3@gmail.com>
2023-07-13 21:22:43 +0300

    done for now

--- Drum Kit/drumKit.js
+++ Drum Kit/drumKit.js
@@ -1,24 +1,25 @@
 // window.addEventListener('type:DOMString', callback: EventListener, capture?:boolean)
-let h = 0;
-window.addEventListener('keydown', function(e) {
+
+window.addEventListener('keydown', keys);
+
+function keys(e) {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-console.log(e.keyCode)
     if(!audio) return; //no need to play any sound for this key press
     else{
         audio.currentTime = 0;
         audio.play();
         key.classList.add("playing");
 
-        if(h != e.keyCode){
-            audio = document.querySelector(`audio[data-key="${h}"]`);
-            key = document.querySelector(`div[data-key="${h}"]`);
-            h = e.keyCode;
-            if(!key)return;
+        function removeTransition(e){
+            if(e.propertyName != 'transform')return;
             else{
-                // audio.stop();
-                key.classList.remove("playing");
+                this.classList.remove('playing');
             }
         }
+
+        const keys = document.querySelectorAll(`.key`);
+        keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
     }
-});
+}
