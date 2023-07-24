fc3a0a048a4b92c4fdf05a2742b883058089f21a
Wengel <112881230+wengel7@users.noreply.github.com>
2023-07-25 00:56:17 +0300

    done with clock

---
+++ clock/clock.js
@@ -0,0 +1,22 @@
+
+
+const cssHr = document.querySelector(".clock-hour");
+const cssMin = document.querySelector(".clock-min");
+const cssSec = document.querySelector(".clock-second");
+
+function setTime(){
+    const now = new Date();
+    const sec = now.getSeconds();
+    const min = now.getMinutes();
+    const hr = now.getHours();
+
+    const hrDeg = ((hr / 60) * 360)+ 90;
+    const minDeg = ((min / 60) * 360)+ 90;
+    const secDeg = ((sec / 60) * 360)+ 90;
+    
+    cssHr.style.transform = `rotate(${hrDeg}deg)`;
+    cssMin.style.transform = `rotate(${minDeg}deg)`;
+    cssSec.style.transform = `rotate(${secDeg}deg)`;
+}
+
+setInterval(setTime, 1000);
